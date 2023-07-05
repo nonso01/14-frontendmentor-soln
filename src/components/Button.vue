20c00ba04a9dd7ae640a817b0f7745e95397deb0
nonso01 <chukwunonsomartin01@gmail.com>
2023-07-05 13:29:33 +0100

    init

---
+++ src/components/Button.vue
@@ -0,0 +1,13 @@
+<script setup>
+	import { ref } from "vue";
+
+	defineProps({
+	text: String,
+	click: Function
+	})
+
+</script>
+
+<template>
+	<button class="btn" @click="click"> {{ text }} </button>
+</template>
