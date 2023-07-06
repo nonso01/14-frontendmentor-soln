387cf5d5b5a3b7d0aca357b9493f7f6a295d533e
nonso01 <chukwunonsomartin01@gmail.com>
2023-07-06 22:15:51 +0100

    worked on the Button component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <script setup>
-	import IconPlus from "./components/IconPlus.vue";
+	import Button from "./components/Button.vue";
+	import IconCart from "./components/IconCart.vue";
 	import { ref, computed } from "vue";
 
 	const log = console.log
 
 	const int = ref(0)
 
-	const a = () => int.value++ && log(int.value)
+	const a = () => {
+	int.value++
+	log(int.value)
+	}
 
 </script>
 
 <template>
-	<IconPlus :handleClick="a"/>
+	<Button :hasIcon=true :handleClick="a" text="button" />
 </template>
--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,15 +1,19 @@
 <script setup>
+	import IconCart from "./IconCart.vue";
+
 	const props = defineProps({
-	text: {
-	type: [String, Number],
-	default: "button",
-	},
+	text: [String, Number],
+	hasIcon: Boolean,
 	handleClick: Function
 	})
 </script>
 
 <template>
-	<button class="btn" @click="handleClick">
-		{{ text }}
+	<button v-if="hasIcon" class="btn" @click="handleClick">
+		<IconCart /> {{ text }}
 	</button>
+
+	<button v-else class="btn" @click="handleClick">
+                {{ text }}
+        </button>
 </template>
